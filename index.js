d9ab8bea3b880970dbf51d923b25a35e36a046c5
Maheshp <maheshp.ug21.ee@nitp.ac.in>
2024-08-19 18:03:46 +0530

    Added few features

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
+let userName="";
+function displayWord() {
+    userName = document.getElementById("userWord").value;
+    helper();
+}
 const helper = async () => {
     try{
-        const res = await fetch("https://codeforces.com/api/user.info?handles=knightstour");
+        const res = await fetch(`https://codeforces.com/api/user.info?handles=${userName}`);
         const data = await res.json();
-        const codeforceApi = document.querySelector("#codeforceApi");
-        codeforceApi.innerHTML =data.result[0].rating;
+        if (data.status == "OK") {
+            if (data.result[0].rating === undefined) {
+                
+                const codeforceApi = document.querySelector("#codeforceApi");
+                codeforceApi.innerHTML =userName;
+                const userTitle = document.querySelector("#userTitle");
+                userTitle.innerHTML = "No Contest by";
+
+            }
+            else {
+                const codeforceApi = document.querySelector("#codeforceApi");
+                codeforceApi.innerHTML =data.result[0].maxRating + " " + data.result[0].maxRank;
+                const userTitle = document.querySelector("#userTitle");
+                userTitle.innerHTML = userName;
+            }
+            
+        }
+        else {
+            const userTitle = document.querySelector("#userTitle");
+            userTitle.innerHTML = "No User";
+            const codeforceApi = document.querySelector("#codeforceApi");
+            codeforceApi.innerHTML ="";
+        }
+        
 
     }
     catch(error) {
         console.log(error);
     }
 }
-window.addEventListener("load",()=>{
-    helper();
+document.addEventListener("DOMContentLoaded", () => {
+    const submitButton = document.getElementById("submitButton");
+    submitButton.addEventListener("click", displayWord);
 });
+
+// window.addEventListener("load",()=>{
+//     helper();
+// });
