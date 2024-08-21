f369369bf3596cdf41c9b47b681d2889ceb8570e
Maheshp <maheshp.ug21.ee@nitp.ac.in>
2024-08-22 01:28:23 +0530

    replace with solvedCount

--- index.js
+++ index.js
@@ -5,31 +5,48 @@ function displayWord() {
 }
 const helper = async () => {
     try{
-        const res = await fetch(`https://codeforces.com/api/user.info?handles=${userName}`);
+        
+        const res = await fetch(`https://codeforces.com/api/problemset.problems`);
         const data = await res.json();
-        if (data.status == "OK") {
-            if (data.result[0].rating === undefined) {
-                
-                const codeforceApi = document.querySelector("#codeforceApi");
-                codeforceApi.innerHTML =userName;
-                const userTitle = document.querySelector("#userTitle");
-                userTitle.innerHTML = "No Contest by";
+        const arr = [];
+        //console.log(data.result.problemStatistics[0]);
+        const cur = data.result.problemStatistics[0].contestId;
+        for (let i = 0; i < 15; i++) {
+            if (data.result.problemStatistics[i].contestId === cur ) {
+                const tmp = {};
+                tmp[data.result.problemStatistics[i].index] = data.result.problemStatistics[i].solvedCount;
+                arr.push(tmp);
+            }
+        }
+        arr.sort((a, b) => a.solvedCount- b.solvedCount);
+        console.log(arr);
 
-            }
-            else {
-                const codeforceApi = document.querySelector("#codeforceApi");
-                codeforceApi.innerHTML =data.result[0].maxRating + " " + data.result[0].maxRank;
-                const userTitle = document.querySelector("#userTitle");
-                userTitle.innerHTML = userName;
-            }
+
+        
+        //const res = await fetch(`https://codeforces.com/api/user.info?handles=${userName}`);
+        // if (data.status == "OK") {
+        //     if (data.result[0].rating === undefined) {
+                
+        //         const codeforceApi = document.querySelector("#codeforceApi");
+        //         codeforceApi.innerHTML =userName;
+        //         const userTitle = document.querySelector("#userTitle");
+        //         userTitle.innerHTML = "No Contest by";
+
+        //     }
+        //     else {
+        //         const codeforceApi = document.querySelector("#codeforceApi");
+        //         codeforceApi.innerHTML =data.result[0].maxRating + " " + data.result[0].maxRank;
+        //         const userTitle = document.querySelector("#userTitle");
+        //         userTitle.innerHTML = userName;
+        //     }
             
-        }
-        else {
-            const userTitle = document.querySelector("#userTitle");
-            userTitle.innerHTML = "No User";
-            const codeforceApi = document.querySelector("#codeforceApi");
-            codeforceApi.innerHTML ="";
-        }
+        // }
+        // else {
+        //     const userTitle = document.querySelector("#userTitle");
+        //     userTitle.innerHTML = "No User";
+        //     const codeforceApi = document.querySelector("#codeforceApi");
+        //     codeforceApi.innerHTML ="";
+        // }
         
 
     }
